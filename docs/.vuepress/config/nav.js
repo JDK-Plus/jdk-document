12c32a3199c45d9dcbde2ef8bb4445a141b2587a
pf <pf@jdk.plus>
2021-10-20 14:35:38 +0800

    调整菜单项

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -6,9 +6,9 @@ module.exports = [
     ]
   },
   { text: 'JDK源码分析', link: '/pages/e3ab59/',
-    items: [
-      { text: 'validator-plus', link: '/pages/13728c/' },
-    ]
+    // items: [
+    //   { text: 'validator-plus', link: '/pages/13728c/' },
+    // ]
   },
   { text: '问答', link: '/pages/9cc27d/' },
   { text: '反馈', link: '/pages/5dfce5/' },
