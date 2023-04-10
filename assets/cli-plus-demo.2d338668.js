17c1432350e4c717c0af7cb8b8dd00a86f4d16ff
p-moon <moon@jdk.plus>
2023-04-10 06:47:41 +0000

    Deploying to gh_pages from @ JDK-Plus/doc@fbcef5e3d12990c4ca02e9e6f9184ca0450e1180 🚀

--- assets/cli-plus-demo.2d338668.js
+++
@@ -1,1 +0,0 @@
-var i="/img/cli-plus-logo.gif",o="/img/cli-plus-demo.png";export{i as _,o as a};
---
+++ assets/cli-plus-demo.850df0cc.js
@@ -0,0 +1,1 @@
+var i="/img/cli-plus-logo.gif",s="/img/cli-plus/cli-weight.gif",g="/img/cli-plus-demo.png";export{i as _,s as a,g as b};
